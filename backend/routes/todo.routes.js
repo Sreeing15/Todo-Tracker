5836a2909a2d3569f398b0657e9afe4e2326edb9
BBS <batthulabhavyasree@gmail.com>
2026-02-02 15:10:47 +0530

    Initial commit - Task Manager

---
+++ backend/routes/todo.routes.js
@@ -0,0 +1,60 @@
+//this will have functions(CURD OPERATIONS) of todo list
+import express, { response } from 'express';
+import Todo from '../models/todo.model.js';
+
+const router=express.Router();//this router is used to handle diff requests for curd operations
+
+//get all todos
+router.get('/',async(req,res)=>{
+  try{
+    const todos=await Todo.find();//can see todos in mongodb
+    res.json(todos);//sends json response back to clients
+  }catch(error){
+    res.status(500).json({message: err.message})
+  }
+})
+
+//add a new todo
+router.post("/",async(req,res)=>{
+  const todo=new Todo({
+    text:req.body.text
+  })
+  try{
+    const newTodo=await todo.save();
+    res.status(201).json(newTodo)
+
+  }catch(error){
+    res.status(400).json({message:err.message})
+
+  }
+})
+
+//update a todo(text and/or completed)
+router.patch("/:id",async(req,res)=>{
+  try{
+    const todo=await Todo.findById(req.params.id);
+    if(!todo) return res.status(404).json({message: "Todo not Found"});
+    if (req.body.text !== undefined){
+      todo.text=req.body.text;
+
+    }
+    if (req.body.completed!==undefined){
+      todo.completed=req.body.completed;
+    }
+    const updatedTodo=await todo.save();
+    res.json(updatedTodo);
+
+  }catch(error){
+    res.status(400).json({message:err.message});
+  }
+})
+
+router.delete("/:id",async(req,res)=>{
+  try{
+    await Todo.findByIdAndDelete(req.params.id);
+    res.json({message:"TODO DELETED"});
+  }catch(error){
+    res.status(500).json({message:err.message});
+  }
+})
+export default router;
